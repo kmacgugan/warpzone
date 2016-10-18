43fef3c3ae29bee8c2c2c726397f201004dc8909
kmacgugan <kmacgugan@chef.io>
2016-10-17 18:03:30 -0700

    Whitelisting with timing in action

--- _build/test/lib/warp_zone/test/wz_jsoninator_tests.erl
+++ _build/test/lib/warp_zone/test/wz_jsoninator_tests.erl
@@ -4,17 +4,36 @@
 
 -compile(export_all).
 
-jiffy_should_decode_node_file_test() ->
-    {TimeMicroS, _Value} = timer:tc(wz_jsoninator, parse_json,  ["/Users/kmacgugan/chef/warp_zone3/data/small.json"]),
-    TimeMs = TimeMicroS/1000,
-    io:fwrite("Time to parse: ~p~n", [TimeMs]),
-    ?assertEqual(TimeMs, 100).
-
 jiffy_should_decode_node_converge_file_test() ->
-    {TimeMicroS, _Value} = timer:tc(wz_jsoninator, parse_json,  ["/Users/kmacgugan/chef/warp_zone3/data/run_converge_small.json"]),
-    TimeMs = TimeMicroS/1000,
-    io:fwrite("Time to parse: ~p~n", [TimeMs]),
-    ?assertEqual(TimeMs, 100).
+    Value = wz_jsoninator_tests:timeParse("/Users/kmacgugan/chef/warp_zone3/data/run_converge_small.json"),
+    timeWhitelist(Value, [["chef_server_fqdn"],
+    [<<"expanded_run_list">>, <<"run_list">>],
+    ["node", "normal", "omnibus"],
+     ["resources"],
+     ["node", "jenkins"]
+     ]).
 
-jiffy_should_decode_large_files_test() ->
-    {timeout, 3600, ?_assertEqual(timer:tc(wz_jsoninator, parse_json, ["/Users/kmacgugan/chef/warp_zone3/data/citylots.json"]), 100)}.
+jiffy_should_decode_node_file_test() ->
+    Value = wz_jsoninator_tests:timeParse("/Users/kmacgugan/chef/warp_zone3/data/small.json"),
+    timeWhitelist(Value, [[<<"expanded_run_list">>, <<"run_list">>]]).
+
+
+
+
+
+% jiffy_should_decode_large_files_test_() ->
+%     {timeout, 36000, ?_assertEqual(100, wz_jsoninator:time_parse("/Users/kmacgugan/chef/warp_zone3/data/citylots.json"))}.
+
+timeParse(Filename) ->
+    F = fun() -> wz_jsoninator:parse_json(Filename) end,
+    {TimeMicroS, Value} = timer:tc(F),
+    TimeMs = TimeMicroS/1000,
+    ?debugFmt( "Time to parse: ~p~n", [TimeMs]),
+    Value.
+
+timeWhitelist(Json, Whitelist) ->
+    F = fun() -> wz_jsoninator:whitelist(Json, Whitelist) end,
+    {TimeMicroS, Value} = timer:tc(F),
+    TimeMs = TimeMicroS/1000,
+    ?debugFmt( "Time to Whitelist: ~p~n", [TimeMs]),
+    Value.
--- apps/warp_zone/src/wz_jsoninator.erl
+++ apps/warp_zone/src/wz_jsoninator.erl
@@ -1,6 +1,6 @@
 -module(wz_jsoninator).
 
--export([read_file/1, parse_json/1]).
+-export([read_file/1, parse_json/1, time_parse/1, whitelist/2]).
 
 read_file(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -14,10 +14,24 @@ get_all_lines(Device, Accum) ->
 
 parse_json(Filename) ->
     JsonFile = read_file(Filename),
-    io:fwrite("Decoding JSON at ~p~n", [erlang:timestamp()]),
     Json = jiffy:decode(JsonFile),
-    io:fwrite("Done with JSON at ~p~n", [erlang:timestamp()]),
     Json.
 
-whitelist(Json, WhiteList) ->
-    
+time_parse(Filename) ->
+    {TimeMicroS, _Value} = timer:tc(fun() -> parse_json(Filename) end),
+    TimeMs = TimeMicroS/1000,
+    TimeMs.
+
+% -type ej_path_element() :: atom() | binary() | string().
+% -type ej_path() :: list(ej_path_element()) | tuple(ej_path_element()).
+% -spec whitelist(string(), list(ej_path())) -> ejson().
+whitelist(Json, Whitelist) ->
+    whitelist(Json, Whitelist, {[]}).
+
+% -spec whitelist(string(), list(ej_path()), list(ejson())) -> ejson().
+whitelist(Json, [ FirstKey | Whitelist], Acc) ->
+    % io:format(user, "Key: ~p", [FirstKey]),
+    % io:format(user, " Value is: ~p~n", [ej:get(FirstKey, Json)]),
+    whitelist(Json, Whitelist, ej:set_p(FirstKey, Acc, ej:get(FirstKey, Json)));
+whitelist(_Json, [], Acc) ->
+    Acc.
